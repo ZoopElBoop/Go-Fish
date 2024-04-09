e2e54e66e862283eb2e2d8957837291326066348
ZoopElBoop <114008497+ZoopElBoop@users.noreply.github.com>
2024-04-09 02:00:52 +0100

    Fraudulent Fixes #4

--- Assets/Scripts/Enviroment/LampChain.cs
+++ Assets/Scripts/Enviroment/LampChain.cs
@@ -12,6 +12,10 @@ public class LampChain : MonoBehaviour
     [Min(2)] public int numberOfPoints;
 
     public float speed;
+    public float timePoints = 0.01f;
+
+    public float DEBUG;
+
 
     private void Update() 
     {
@@ -29,26 +33,34 @@ public class LampChain : MonoBehaviour
         if (Line.positionCount == 2)
             return;
 
+
         float distanceBetween = (float) 1 / (numberOfPoints - 1);
+        int pointsInside = Line.positionCount - 2;
+        int midPoint = Mathf.CeilToInt(pointsInside / 2) + 1;
 
         Vector3 velocity = speed * -transform.up;
         float time = 0f;
 
         for (int i = 1; i < numberOfPoints - 1; i++)
         {
+            if (i <= midPoint)
+                time += Mathf.InverseLerp(midPoint, 1, i) * timePoints;
+            else
+                time -= Mathf.InverseLerp(midPoint, numberOfPoints - 1, i) * timePoints;
+
             var y = (velocity.y * time) + (Physics.gravity.y / 2 * time * time);
 
             Vector3 point = GetPoint(transform.position, to.position, distanceBetween * i);
 
             point = new Vector3(
                 point.x,
-                point.y - 2f,
+                point.y + y,
                 point.z
                 );
 
             Line.SetPosition(i, point);
-            faaaaaarom.position = point;
         }
+        faaaaaarom.position = Line.GetPosition(midPoint);
     }
 
     private Vector3 GetPoint(Vector3 a, Vector3 b, float point) 
